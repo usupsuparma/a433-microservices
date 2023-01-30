70b0541a8cadc4a7fed54f6f1ab80e238a26815e
usupsuparma <usup.suparma7@gmail.com>
2023-01-31 00:41:25 +0700

    change to v1

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,5 +1,5 @@
 # membuat image dengan Tag v1
-docker build -t item-app:v2 .
+docker build -t item-app:v1 .
 
 
 # Melihat semua daftar images
@@ -7,11 +7,11 @@ docker images
 
 
 # Merubah nama image
-docker tag item-app:v2 usupsuparma/item-app:v2
+docker tag item-app:v1 usupsuparma/item-app:v1
 
 # Login ke Docker Hub
 # docker login -u %DOCKER_USERNAME% -p %DOCKER_PASSWORD%
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 # Push image ke Docker Hub
-docker push usupsuparma/item-app:v2
+docker push usupsuparma/item-app:v1
