8622b89c6861c0c41c08380b2e59f4da8bb3d4a4
usupsuparma <usup.suparma7@gmail.com>
2023-01-31 00:40:31 +0700

    fix: merubah nama image

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,15 +1,17 @@
-# membuat image
-docker build -t item-app .
+# membuat image dengan Tag v1
+docker build -t item-app:v2 .
 
-# Membuat Tag V1
-docker tag item-app usupsuparma/item-app:v1
 
 # Melihat semua daftar images
 docker images
+
+
+# Merubah nama image
+docker tag item-app:v2 usupsuparma/item-app:v2
 
 # Login ke Docker Hub
 # docker login -u %DOCKER_USERNAME% -p %DOCKER_PASSWORD%
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 # Push image ke Docker Hub
-docker push usupsuparma/item-app:v1
+docker push usupsuparma/item-app:v2
